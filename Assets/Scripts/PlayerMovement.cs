4a0fbdcc3acfd9d6d07940fa5ed565e498ffb26e
Anton Meluta <meluta.anton@yandex.ru>
2021-12-20 06:24:21 +0700

    First commit. Наброски первых скриптов и сцены.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private NavMeshAgent meshAgent;
+    private Animator animator;
+
+    private void Awake()
+    {
+        meshAgent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+    public void GoTargetMoving(Vector3 destination)
+    {
+        //meshAgent.destination = destination;
+    }
+
+    private void Update()
+    {
+
+        animator.SetFloat("speedMove", meshAgent.velocity.magnitude);
+        if (Input.GetKeyDown(KeyCode.E))
+            animator.SetBool("isWalk", true);
+        if (Input.GetKeyUp(KeyCode.E))
+            animator.SetBool("isWalk", false);
+    }
+}
---
+++ Assets/Scripts/TapUiControl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TapUiControl : MonoBehaviour, IDragHandler, IPointerDownHandler, 
+    IPointerUpHandler
+{
+    public PlayerMovement playerMovement; //событие вызывать
+    public IkArmControl ikArmControl; //событие вызывать
+
+    public LayerMask layersClickable;
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, layersClickable))
+        {
+            playerMovement.GoTargetMoving(hit.point);
+        }
+
+        // Таргетирование для стрельбы, вызов событий
+        ikArmControl.TryShootingOn();
+        ikArmControl.CubeCostil(hit.point);     //событие передвижения цели кинематик-анимации руки
+        //Вызов события для стрельбы
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, layersClickable))
+        {
+            playerMovement.GoTargetMoving(hit.point);
+        }
+
+        ikArmControl.TryShootingOn();
+        ikArmControl.CubeCostil(hit.point);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        // Таргетирование для стрельбы, вызов события
+        ikArmControl.TryShootingOff();
+    }
+}
